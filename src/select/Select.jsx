01e4c8007a427c31b2f4f814d0095771a86825a1
mytharcher <mytharcher@gmail.com>
2018-08-31 00:40:21 +0800

    Fix select name property (fix #9).

--- src/select/Select.jsx
+++ src/select/Select.jsx
@@ -818,7 +818,7 @@ class Select extends Component {
   }
 
   render() {
-    const { multiple, size, disabled, filterable, loading } = this.props;
+    const { multiple, size, disabled, filterable, loading, name } = this.props;
     const { selected, inputWidth, inputLength, query, selectedLabel, visible, options, filteredOptionsCount, currentPlaceholder } = this.state;
 
     return (
@@ -899,7 +899,7 @@ class Select extends Component {
           value={selectedLabel}
           type="text"
           placeholder={currentPlaceholder}
-          name="name"
+          name={name}
           size={size}
           disabled={disabled}
           readOnly={!filterable || multiple}
